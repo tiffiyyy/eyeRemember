29c0746926a8009ce5b86781332a881852d2d087
tiffiyyy <Tiffany.Le710@outlook.com>
2025-05-17 00:56:40 -0700

    "refactoring"
    qa

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ import cv2
 import matplotlib.pyplot as plt
 import numpy as np
 from openvino.runtime import Core
+from face_detector import detect_faces
 
 ie = Core()
 
@@ -20,33 +21,11 @@ input_layer_ir = compiled_model.input(0)
 output_layer_ir = compiled_model.output(0)
 
 image = cv2.imread("data/1-face.jpg")
-height, width = image.shape[0:2]
 
-# n = num of images, c = channels (colors detected)
-# h = image height, w = image width 
-N, C, H, W = input_layer_ir.shape 
-resized_image = cv2.resize(image, (W, H))
+# generates a matrix of vectors containing facial information and 
+# filters out entries with low confidence level 
+boxes = detect_faces(image, 0.3)
 
-# image is resized for model to process images in format: (1, W, C, H)
-input_image = np.expand_dims(resized_image.transpose(2, 0, 1), 0)
-
-plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-# threshold value; used to filter out faces with confidence level < 0.3 
-THRESH = 0.3 
-
-# generates a matrix of vectors containing facial information 
-# format: image_id, label, confidence level, coordinates of box 
-boxes = compiled_model([input_image])[output_layer_ir]
-
-# removes image_id and label values from each vector in boxes 
-boxes = boxes.squeeze()[:, -5:]
-# for each x in boxes, the last four values will be kept 
-# if the confidence level is greater than the threshold value 
-boxes = np.array([x[-4:] for x in boxes if x[0] > THRESH])
-
-# restore original image dimensions to be returned with box on detected face 
-boxes = boxes * np.array([width, height, width, height])
 
 # function to draw rectangle around detected face 
 def draw(image, boxes): 
