d4b8cdef1fc99547695aa405c8ac40a9f0d7d37f
linuswong <linuswong9@gmail.com>
2025-05-17 13:16:04 -0700

    Created utils.py and it contains comparing functions as well as a saveImage function

--- backend/src/face_reid.py
+++ backend/src/face_reid.py
@@ -21,7 +21,13 @@ output_layer_ir = compiled_model.output(0)
 
 
 N, C, H, W = input_layer_ir.shape 
+    #extract from image upload
 image = cv2.imread("data/IMG_2875.jpeg")
+
+def getW():
+    return W
+def getH():
+    return H
 
 def preprocess(image:np.ndarray,threshold:float,width:int,height:int):
     # generates boxes for each face detected 
@@ -34,24 +40,9 @@ def preprocess(image:np.ndarray,threshold:float,width:int,height:int):
         x_min, y_min, x_max, y_max = box
         face_crop = image[y_min:y_max, x_min:x_max]
         resizedReid = cv2.resize(face_crop,(W,H))
-        # resize image so that model can process a
+        # resize image so that model can process it
         input_blob = np.expand_dims(resizedReid.transpose(2, 0, 1), axis=0).astype(np.float32)
         # passes image through model; returns model ouput into an array (embeddings)
         embeddings.append(compiled_model([input_blob])[output_layer_ir])
     return embeddings
-
-embeddings = preprocess(image, threshold=0.3, width=W, height=H)
-
-# for every image processed, generate an image ID for each person 
-
-name = "linus"
-
-# creates a dictionary of profiles linking a name to an embedding 
-profiles = {
-    name: embeddings
-}
-
-# populates a pickle file with all profiles 
-with open("face_db.pkl", "wb") as f:
-    pickle.dump(profiles, f)
 
---
+++ backend/src/utils.py
@@ -0,0 +1,38 @@
+import numpy as np
+from face_reid import preprocess, getH, getW
+import pickle
+
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
+
+def find_best_match(embedding, face_db, threshold):
+    best_score = -1
+    best_match = "Unknown"
+    for name, db_embedding in face_db.items():
+        score = cosine_similarity(embedding, db_embedding)
+        if score > best_score and score > threshold:
+            best_score = score
+            best_match = name
+    return best_match
+
+# for every image processed, generate an image ID for each person 
+    #extract name from frontend name upload, should linked with the previously extracted image
+
+def save_face_db(image,name,desc):
+    embeddings = preprocess(image, threshold=0.3, width=getW(), height=getH())
+    name = name
+    desc= desc
+
+
+    try:
+        with open("data/face_db.pkl", "rb") as f:
+            profiles = pickle.load(f)
+    except FileNotFoundError:
+        profiles = {}
+
+    if embeddings:
+        profiles[name] = embeddings[0]
+    
+    # populates a pickle file with all profiles 
+    with open("face_db.pkl", "wb") as f:
+        pickle.dump(profiles, f)
